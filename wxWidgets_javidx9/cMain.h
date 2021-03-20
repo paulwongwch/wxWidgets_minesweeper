2022ba0bd9e907e0fc941d838871b602569db80a
kdtchw <kdtchw@gmail.com>
2021-03-20 22:15:03 +0800

    first commit

---
+++ wxWidgets_javidx9/cMain.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "wx/wx.h"
+
+class cMain : public wxFrame
+{
+public:
+	cMain();
+	~cMain();
+
+private:
+	int nFieldWidth = 10;
+	int nFieldHeight = 10;
+	wxButton** btn;
+
+	int* nField = nullptr;
+	bool bFirstClick = true;
+
+	void OnButtonClicked(wxCommandEvent &evt);
+
+	wxDECLARE_EVENT_TABLE();
+};
+
